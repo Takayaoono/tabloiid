a93d41b12b45b3aedb4e38a272ea5d50a4252fa6
ks0222 <k-sawada@iid.co.jp>
2022-03-11 12:05:29 +0900

    fix: routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,13 @@ Rails.application.routes.draw do
   #resources :users
   #resources :guests
   resources :tags
-  #resources :posts #Postのソート機能の時に別のアクションを呼び出してしまうのでpost_controllerのルーティングを自分で設定
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/create'
+  resources :posts #Postのソート機能の時に別のアクションを呼び出してしまうのでpost_controllerのルーティングを自分で設定
+
   get 'tags/search/:id' => 'tags#search' #タグ絞り込み検索用ルーティング
   get 'tags_reset' => 'tags#reset' #タグ絞り込みの「全て表示」用ルーティング
   get 'tag_edit/:post_id' => 'tags#edit' #1つのpostに対してのタグの追加・削除を行うモーダル用ルーティング
   delete 'tag_edit/:post_id/:tag_id' => 'tags#destroy_2' #1つのpostに対してのタグの削除を行うルーティング
   post 'tag_edit/:post_id/:tag_id' => 'tags#create_2' #1つのpostに対してのタグの追加を行うルーティング
+
   root 'guests#index'
 end
